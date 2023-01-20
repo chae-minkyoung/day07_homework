1a58e5b940184c23293ed15e92416bc6275bb4ef
chae minkyoung <chea220215@gmail.com>
2023-01-20 19:05:44 +0900

    10.9

--- day07 homework.py
+++ day07 homework.py
@@ -1,18 +1,24 @@
-#10.8
-class Element:
-    def __init__(self,name,symbol,number):
-        self.__name = name
-        self.__symbol = symbol
-        self.__number = number
-    def setter(self,input_name):
-        self.__name=input_name
-    def getter(self):
-        print(self.__name)
-hydrogen=Element('Hydrogen','H','1')
+#10.9
+class Bear():
+    def eats(self):
+        return 'berries'
+class Rabbit():
+    def eats(self):
+        return 'clover'
+class Octothorpe:
+    def eats(self):
+        return 'campers'
+A=Bear()
+B=Rabbit()
+C=Octothorpe()
 
-hydrogen.getter()
-hydrogen.setter('Argon')
-hydrogen.getter()
+print(A.eats())
+print(B.eats())
+print(C.eats())
+
+
+
+
 
 
 
