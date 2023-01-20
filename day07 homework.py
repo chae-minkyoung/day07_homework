0bce586e6c62bdc29d2c9c1f0eeefbd6e98672d8
chae minkyoung <chea220215@gmail.com>
2023-01-20 18:44:26 +0900

    10.7

--- day07 homework.py
+++ day07 homework.py
@@ -1,13 +1,13 @@
-#10.6
+#10.7
 class Element:
     def __init__(self,name,symbol,number):
         self.name = name
         self.symbol = symbol
         self.number = number
-    def dump(self):
-        print('self.name, self.symbol, self.number')
+    def __str__(self):
+        return ('self.name, self.symbol, self.number')
 hydrogen=Element('Hydrogen','H','1')
 
-hydrogen.dump()
+print(hydrogen.__str__())
 
 
